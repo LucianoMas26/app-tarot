5835cce48f6eba8fa1044264ee4ef95b9eadf7b1
Luciano <lucianoalejandromas@gmail.com>
2024-10-31 22:57:52 -0300

    adding dependencies

--- app/libs/mongodb.ts
+++ app/libs/mongodb.ts
@@ -1,42 +1,19 @@
 import mongoose from "mongoose"
 
-declare global {
-  var mongoose: {
-    conn: mongoose.Connection | null
-    promise: Promise<mongoose.Connection> | null
-  }
-}
-
-const MONGODB_URL: string =
+const URL: string =
   process.env.MONGODB_URL ||
   "mongodb+srv://lucianomas:37450853@cluster0.l79bm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-if (!MONGODB_URL) {
+if (!URL) {
   throw new Error("MONGODB_URL environment variable is not defined.")
 }
 
-let cached = global.mongoose as any
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
-}
-
 export async function connectDB() {
-  if (cached.conn) {
-    return cached.conn
+  try {
+    await mongoose.connect(URL)
+    console.log("Successfully connected to MongoDB")
+  } catch (error: any) {
+    console.error("Error connecting to MongoDB:", error.message)
+    throw new Error("MongoDB connection failed")
   }
-
-  if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(MONGODB_URL)
-      .then((mongoose) => mongoose)
-      .catch((error) => {
-        console.error("Error connecting to MongoDB:", error.message)
-        throw new Error("MongoDB connection failed")
-      })
-  }
-
-  cached.conn = await cached.promise
-  console.log("Successfully connected to MongoDB")
-  return cached.conn
 }
